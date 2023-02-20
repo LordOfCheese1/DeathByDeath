c7de3a170baecd247354ce96cba5bb300d61d4ca
LordOfCheese <94697288+LordOfCheese1@users.noreply.github.com>
2023-02-20 22:54:26 +0100

    Decoration

--- Prefabs/Entities/MaxHealthPickup/MaxHealthPickup.gd
+++ Prefabs/Entities/MaxHealthPickup/MaxHealthPickup.gd
@@ -11,5 +11,6 @@ func _on_MaxHealthPickup_body_entered(body):
 		$AnimationPlayer.play("Collect")
 		yield(get_tree().create_timer(0.5, false), "timeout")
 		Values.player_max_health += 5
+		Values.player.heal(Values.player_max_health - Values.player_health)
 		Values.collected_healthups.append(name)
 		call_deferred("free")

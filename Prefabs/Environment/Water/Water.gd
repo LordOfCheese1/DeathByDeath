272d66831dd956e98eb9c8ec61e0431f86093477
LordOfCheese <94697288+LordOfCheese1@users.noreply.github.com>
2023-02-27 20:47:47 +0100

    Water level

--- Prefabs/Environment/Water/Water.gd
+++ Prefabs/Environment/Water/Water.gd
@@ -1,5 +1,13 @@
 extends Area2D
 
 
+export var water_size : Vector2
+var collidershape = RectangleShape2D.new()
+
+
 func _ready():
 	add_to_group("water")
+	collidershape.extents = water_size / 2
+	$WaterTile.scale = water_size / 16
+	$CollisionShape2D.shape = collidershape
+	$WaterTile.material.set_shader_param("tile", water_size / 16)
